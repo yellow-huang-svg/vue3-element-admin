b28f7769cd2f6731d2282b6c40b6f81b00edda48
yellow-huang-svg <84072842+yellow-huang-svg@users.noreply.github.com>
2024-09-13 00:01:39 +0800

    项目初始化

---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+import HelloWorld from '@/components/HelloWorld.vue'
+</script>
+
+<template>
+  <el-button type="success"><i-ep-SuccessFilled />Success</el-button>
+  <el-button type="info"><i-ep-InfoFilled />Info</el-button>
+  <el-button type="warning"><i-ep-WarningFilled />Warning</el-button>
+  <el-button type="danger"><i-ep-WarnTriangleFilled />Danger</el-button>
+</template>
+
+<style scoped>
+</style>
---
+++ vite.config.ts
@@ -0,0 +1,61 @@
+import { UserConfig, ConfigEnv, loadEnv, defineConfig } from "vite";
+import vue from '@vitejs/plugin-vue'
+import { resolve } from "path";
+import AutoImport from "unplugin-auto-import/vite";
+import Components from "unplugin-vue-components/vite";
+import { ElementPlusResolver } from "unplugin-vue-components/resolvers";
+import Icons from "unplugin-icons/vite";
+import IconsResolver from "unplugin-icons/resolver";
+import { createSvgIconsPlugin } from 'vite-plugin-svg-icons';
+
+const pathSrc = resolve(__dirname, "src");
+export default defineConfig(({ mode }: ConfigEnv): UserConfig => {
+  const env = loadEnv(mode, process.cwd());
+  return {
+    resolve: {
+      alias: {
+        "@": pathSrc,
+      },
+    },
+    plugins: [
+      vue(),
+      AutoImport({
+        // 自动导入 Vue 相关函数，如：ref, reactive, toRef 等
+        imports: ["vue"],
+        eslintrc: {
+          enabled: false, // 是否自动生成 eslint 规则，建议生成之后设置 false 
+          filepath: "./.eslintrc-auto-import.json", // 指定自动导入函数 eslint 规则的文件
+        },
+        resolvers: [
+          // 自动导入 Element Plus 相关函数，如：ElMessage, ElMessageBox... (带样式)
+          ElementPlusResolver(),
+          // 自动导入图标组件
+          IconsResolver({}),
+        ],
+        vueTemplate: true, // 是否在 vue 模板中自动导入
+        dts: resolve(pathSrc, "types", "auto-imports.d.ts"), // 指定自动导入函数TS类型声明文件路径
+      }),
+      Components({
+        resolvers: [
+          // 自动导入 Element Plus 组件
+          ElementPlusResolver(),
+          // 自动注册图标组件
+          IconsResolver({
+            enabledCollections: ["ep"] // element-plus图标库，其他图标库 https://icon-sets.iconify.design/
+          }),
+        ],
+        dts: resolve(pathSrc, "types", "components.d.ts"), // 指定自动导入组件TS类型声明文件路径
+      }),
+      createSvgIconsPlugin({
+        // 指定需要缓存的图标文件夹
+        iconDirs: [resolve(process.cwd(), 'src/assets/icons')],
+        // 指定symbolId格式
+        symbolId: 'icon-[dir]-[name]',
+      }),
+      Icons({
+        // 自动安装图标库
+        autoInstall: true,
+      }),
+    ]
+  }
+})
