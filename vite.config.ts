ee7dbd59d8834a40c86fb44f16ae5f284a238f45
yellow-huang-svg <84072842+yellow-huang-svg@users.noreply.github.com>
2024-10-17 23:48:29 +0800

    修改esint配置

--- vite.config.ts
+++ vite.config.ts
@@ -41,16 +41,19 @@ export default defineConfig(({ mode }: ConfigEnv): UserConfig => {
       AutoImport({
         // 自动导入 Vue 相关函数，如：ref, reactive, toRef 等
         imports: ["vue", "@vueuse/core", "pinia", "vue-router"],
-        eslintrc: {
-          enabled: false, // 是否自动生成 eslint 规则，建议生成之后设置 false 
-          filepath: "./.eslintrc-auto-import.json", // 指定自动导入函数 eslint 规则的文件
-        },
         resolvers: [
           // 自动导入 Element Plus 相关函数，如：ElMessage, ElMessageBox... (带样式)
           ElementPlusResolver(),
           // 自动导入图标组件
           IconsResolver({}),
         ],
+        eslintrc: {
+          // 是否自动生成 eslint 规则，建议生成之后设置 false
+          enabled: false,
+          // 指定自动导入函数 eslint 规则的文件
+          filepath: "./.eslintrc-auto-import.json",
+          globalsPropValue: true,
+        },
         vueTemplate: true, // 是否在 vue 模板中自动导入
         dts: resolve(pathSrc, "types", "auto-imports.d.ts"),  // 指定自动导入函数TS类型声明文件路径 (false:关闭自动生成)
       }),
